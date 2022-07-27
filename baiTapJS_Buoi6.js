0aee5048d11ccc2cd2f6419ef878fa1237e43120
baovietctg <baovietctg@gmail.com>
2022-07-27 20:29:14 +0700

    Hoan thanh bai tap

--- baiTapJS_Buoi6.js
+++ baiTapJS_Buoi6.js
@@ -2,10 +2,10 @@
  * Bài tập 1 - tím số nguyên dương n nhỏ nhất sao cho:
  * 1 + 2 + ... + n > 10.000
  */
-function timSo(){
+function timSo() {
     var sum = 0;
     for (var i = 0; i < 10000; i++) {
-        if (sum < 10000){
+        if (sum < 10000) {
             sum += i;
             document.getElementById("txtResultbai1").innerHTML = "Số nguyên dương nhỏ nhất là: " + i;
         }
@@ -17,12 +17,12 @@ timSo();
  * Bài tập 2: nhập só x và số n, tính tổng: S(n) = x + x^2 + x^3 + ... + x^n
  */
 
-function tinhSn(){
+function tinhSn() {
     var x = Number(document.getElementById("soX").value);
     var n = Number(document.getElementById("soN").value);
     var sum = 0;
-    for (var i = 1; i < n+1; i++) {
-        sum = sum + Math.pow(x,i)
+    for (var i = 1; i < n + 1; i++) {
+        sum = sum + Math.pow(x, i)
         console.log(sum)
     }
     document.getElementById("txtResultbai2").innerHTML = "Tổng: " + sum;
@@ -33,10 +33,10 @@ document.getElementById("tinhBai2").onclick = tinhSn;
  * Bài tập 3: nhập só n, tính n!
  */
 
-function tinhGiaiThua(){
+function tinhGiaiThua() {
     var n = Number(document.getElementById("soN2").value);
     var tich = 1;
-    for (let i = 2; i < n+1; i++) {
+    for (let i = 2; i < n + 1; i++) {
         tich = tich * i;
         console.log(tich);
         document.getElementById("txtResultbai3").innerHTML = n + "! = " + tich;
@@ -49,18 +49,66 @@ document.getElementById("tinhBai3").onclick = tinhGiaiThua;
  * Bài tập 4: tạo Div
  */
 
-function taoDiv(){
+function taoDiv() {
     var content = "";
-    // var chan = document.getElementById("div").style.Background.Red;
-    // var le = document.getElementById("div").style.Background.Blue;
 
-    for (let i = 1; i < 11; i++){
-        if(i % 2 == 0){
-            content = '<div style="background:red; margin: 5px; padding:5px; color:#FFF">' + content + '</div>';
-        }else{
-            content = '<div style="background:blue; margin: 5px; padding:5px; color:#FFF">' + content + '</div>';
+    for (let i = 1; i < 11; i++) {
+        if (i % 2 == 0) {
+            content = content + "<p style='background-color: blue; color: white; margin: 0px; padding: 0px'>div chẵn</p>"
+        } else {
+            content = content + "<p style='background-color: red; color: white; margin: 0px; padding: 0px'>div lẻ</p>"
         }
     }
     document.getElementById("txtResultbai4").innerHTML = content;
 }
 document.getElementById("tinhBai4").onclick = taoDiv;
+
+/**
+ * In số nguyên tố
+ */
+
+// function soNguyenTo() {
+//     var num = Number(document.getElementById("soNguyenTo").value);
+//     var content = "";
+//         for (var i = 1; i < num + 1; i++) {
+//             for (var j = 2; j <= Math.sqrt(num); j++){
+//                 if (i % j != 0){
+//                     content = content + i;
+//                 }
+//             }
+//         }
+//     console.log(content);
+// }
+// document.getElementById("tinhBai5").onclick = soNguyenTo;
+
+
+function kiemTraSo(num) {
+    var ketqua = true;
+    
+    if (num < 2) {
+        ketqua = false;
+    } else if (num == 2) {
+        ketqua = true;
+    }else if (num % 2 == 0) {
+        ketqua = false;
+    }else {
+        for (var i = 3; i <=  Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return  false;
+            }
+        }
+    }
+    return ketqua;
+}
+function inSoNT() {
+    var n = document.getElementById("soNguyenTo").value;
+    var content = '';
+
+    for (var i = 1; i <= n; i++) {
+        if (kiemTraSo(i)) {
+            content += "  " + i;
+        }
+        document.getElementById("txtResultbai5").innerHTML = content;
+    }
+}
+document.getElementById("tinhBai5").onclick = inSoNT;
